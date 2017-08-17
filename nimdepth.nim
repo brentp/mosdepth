05a8438c634c97576ee97f04ba63606738202506
Brent Pedersen <bpederse@gmail.com>
2017-08-17 15:58:58 -0600

    --regions

--- nimdepth.nim
+++ nimdepth.nim
@@ -16,7 +16,7 @@ proc dump(arr: seq[uint16], chrom: string) =
     last_start = i
     last_cov = cov
 
-proc inc_coverage*(c: Cigar, ipos: int = 0, arr: var seq[uint16], qname: string="") =
+proc inc_coverage*(c: Cigar, ipos: int = 0, arr: var seq[uint16], qname: string="") {. inline .} =
   if c.len == 1 and c[0].op == CigarOp(match):
     for i in ipos..<(ipos + c[0].len):
        arr[i] += 1
@@ -39,16 +39,23 @@ proc inc_coverage*(c: Cigar, ipos: int = 0, arr: var seq[uint16], qname: string=
   for i in last.start..<last.stop:
     arr[i] += 1
 
-proc main(path: string, threads:int=0, mapq:int= -1) =
+iterator regions(bam: Bam, region: string): Record =
+  if region == nil:
+    for r in bam:
+      yield r
+  else:
+    for r in bam.querys(region):
+      yield r
+
+proc main(path: string, threads:int=0, mapq:int= -1, region: string = "") =
   var bam = hts.open_hts(path, threads=threads, index=true)
   var seqs = bam.hdr.targets
 
   var tgt: hts.Target
   var arr: seq[uint16]
-
   #var heap = bh.new_heap[hts.Range]() do (a, b: Range) -> int:
   #  return a.start - b.start
-  for rec in bam.query("chr1", 0, 190000000):
+  for rec in bam.regions(region):
     if rec.flag.has_flag(BAM_FUNMAP or BAM_FQCFAIL or BAM_FDUP or BAM_FSECONDARY): continue
     if int(rec.qual) < mapq: continue
     if tgt == nil or tgt.tid != rec.b.core.tid:
@@ -61,7 +68,6 @@ proc main(path: string, threads:int=0, mapq:int= -1) =
   if tgt != nil:
     dump(arr, tgt.name)
 
-
 when(isMainModule):
 
   let doc = """
@@ -71,10 +77,11 @@ when(isMainModule):
   
   -t --threads <threads>  number of threads to use [default: 1]
   -Q --mapq <mapq>        mapping quality threshold [default: 0]
+  -r --region <region>    region to restrict depth calc.
   -h --help               show help
   """
 
   let args = docopt(doc, version = "nimdepth 0.1.1")
   var mapq = S.parse_int($args["--mapq"])
 
-  main($args["<BAM>"], S.parse_int($args["--threads"]), mapq)
+  main($args["<BAM>"], S.parse_int($args["--threads"]), mapq, region=($args["--region"]))
