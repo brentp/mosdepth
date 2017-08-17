30922db18887fa2a86849524183b4c3f696ff707
Brent Pedersen <bpederse@gmail.com>
2017-08-17 15:21:31 -0600

    depth calling based on incrementing an array

--- nimdepth.nim
+++ nimdepth.nim
@@ -12,12 +12,35 @@ proc dump(arr: seq[uint16], chrom: string) =
     if cov == last_cov: continue
     if last_cov != 0:
       echo chrom, "\t", intToStr(last_start), "\t", intToStr(i), "\t", $last_cov
+
     last_start = i
     last_cov = cov
 
+proc inc_coverage*(c: Cigar, ipos: int = 0, arr: var seq[uint16], qname: string="") =
+  if c.len == 1 and c[0].op == CigarOp(match):
+    for i in ipos..<(ipos + c[0].len):
+       arr[i] += 1
+    return
+
+  var pos = ipos
+  var last: Range = (0, 0)
+  for op in c:
+    if not op.consumes_reference:
+      continue
+    var olen = op.len
+    if op.consumes_query:
+      if pos != last.stop:
+        for i in last.start..<last.stop:
+          arr[i] += 1
+        last = (pos, pos+olen)
+      else:
+        last.stop = pos + olen
+    pos += olen
+  for i in last.start..<last.stop:
+    arr[i] += 1
+
 proc main(path: string, threads:int=0, mapq:int= -1) =
-  echo mapq
-  var bam = hts.open_hts(path, threads=threads)
+  var bam = hts.open_hts(path, threads=threads, index=true)
   var seqs = bam.hdr.targets
 
   var tgt: hts.Target
@@ -25,17 +48,16 @@ proc main(path: string, threads:int=0, mapq:int= -1) =
 
   #var heap = bh.new_heap[hts.Range]() do (a, b: Range) -> int:
   #  return a.start - b.start
-  for rec in bam:
-    if rec.flag.unmapped: continue
+  for rec in bam.query("chr1", 0, 190000000):
+    if rec.flag.has_flag(BAM_FUNMAP or BAM_FQCFAIL or BAM_FDUP or BAM_FSECONDARY): continue
     if int(rec.qual) < mapq: continue
     if tgt == nil or tgt.tid != rec.b.core.tid:
         if tgt != nil:
           dump(arr, tgt.name)
         tgt = seqs[rec.b.core.tid]
         arr = new_seq[uint16](tgt.length)
-    for piece in rec.cigar.ref_coverage(rec.start):
-      for i in piece.start..<piece.stop:
-        arr[i]+=1
+    inc_coverage(rec.cigar, rec.start, arr, rec.qname)
+
   if tgt != nil:
     dump(arr, tgt.name)
 
