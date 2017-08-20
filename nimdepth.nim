dec6b77ce48982b14ca294fee4f99512c7b9fe60
Brent Pedersen <bpederse@gmail.com>
2017-08-19 21:58:48 -0600

    fully working overlap correction. accounts for all cigar ops

--- nimdepth.nim
+++ nimdepth.nim
@@ -1,6 +1,8 @@
 import hts as hts
 import tables
 import strutils as S
+import algorithm as alg
+import sequtils as sequtils
 import os
 import docopt
 import tables
@@ -22,29 +24,36 @@ proc dump(arr: seq[int16], chrom: string) =
     last_start = i
     last_depth = depth
 
-proc inc_coverage*(c: Cigar, ipos: int = 0, arr: var seq[int16]) {. inline .} =
-  if c.len == 1 and c[0].op == CigarOp(match):
-    inc(arr[ipos])
-    dec(arr[ipos + c[0].len])
-    return
+type
+  pair = tuple[pos: int, value: int16]
 
-  var pos = ipos
-  var last_stop = 0
-  for op in c:
-    if not op.consumes_reference:
-      continue
-    var olen = op.len
-    if op.consumes_query:
-      if pos != last_stop:
-        inc(arr[pos]) # increment the incoming pos
-        if last_stop != 0:
-          dec(arr[last_stop]) # and the last stop before overwriting
+proc pair_sort(a, b: pair): int =
+   return a.pos - b.pos
+
+iterator gen_start_ends(c: Cigar, ipos: int): pair =
+  if c.len == 1 and c[0].op == CigarOp(match):
+    yield (ipos, int16(1))
+    yield (ipos + c[0].len, int16(-1))
+  else:
+    var pos = ipos
+    var last_stop = 0
+    for op in c:
+      if not op.consumes_reference:
+        continue
+      var olen = op.len
+      if op.consumes_query:
+        if pos != last_stop:
+          yield (pos, int16(1))
+          if last_stop != 0:
+            yield (last_stop, int16(-1))
         last_stop = pos + olen
-      else:
-        last_stop = pos + olen
-    pos += olen
-  if last_stop != 0:
-    dec(arr[last_stop])
+      pos += olen
+    if last_stop != 0:
+      yield (last_stop, int16(-1))
+
+proc inc_coverage(c: Cigar, ipos: int = 0, arr: var seq[int16]) {. inline .} =
+  for p in gen_start_ends(c, ipos):
+    arr[p.pos] += p.value
 
 iterator regions(bam: Bam, region: string): Record =
   if region == nil:
@@ -87,12 +96,40 @@ proc main(path: string, threads:int=0, mapq:int= -1, region: string = "") =
           # mate:   --------------
           # rec:             ------------
           # decrement:       -----
-          dec(arr[rec.start])
-          inc(arr[mate.stop])
-          #assert rec.start < mate.stop
-          #stderr.write_line rec, " -> ", rec.cigar
-          #stderr.write_line mate, " -> ", rec.cigar
-          #stderr.write_line rec.start, "...", mate.stop
+          if rec.b.core.n_cigar == 1 and mate.b.core.n_cigar == 1:
+            dec(arr[rec.start])
+            inc(arr[mate.stop])
+          else:
+            # track the overlaps of pair.
+            # anywhere there is overlap, the cumulative sum of pair.depth will be 2. we dec the start and inc the end of the overlap.
+            # this removes the double counting.
+            # e.g.:
+            # (pos: 4623171, value: 1)(pos: 4623223, value: 1)(pos: 4623240, value: -1)(pos: 4623241, value: 1)(pos: 4623264, value: -1)(pos: 4623320, value: -1)
+            # would dec the following intervals:
+            # 4623223 4623240
+            # 4623241 4623264
+            # chr1 4623171 69M1D23M9S (pos: 4623171, value: 1)(pos: 4623241, value: 1)(pos: 4623240, value: -1)(pos: 4623264, value: -1)
+            # chr1 4623223 4S97M (pos: 4623223, value: 1)(pos: 4623320, value: -1)
+            assert rec.start < mate.stop
+            # each element will have a .value of 1 for start and -1 for end.
+
+            var ses = sequtils.to_seq(gen_start_ends(rec.cigar, rec.start))
+            for p in gen_start_ends(mate.cigar, mate.start):
+                ses.add(p)
+            alg.sort(ses, pair_sort)
+            var pair_depth = 0
+            var last_pos = 0
+            #if len(ses) > 4: stderr.write_line ses
+            for p in ses:
+              assert pair_depth <= 2
+              if p.value == -1 and pair_depth == 2:
+                #if len(ses) > 4: stderr.write_line last_pos, " ", p.pos
+                dec(arr[last_pos])
+                inc(arr[p.pos])
+              pair_depth += p.value
+              last_pos = p.pos
+            assert pair_depth == 0
+
     inc_coverage(rec.cigar, rec.start, arr)
 
   if tgt != nil:
