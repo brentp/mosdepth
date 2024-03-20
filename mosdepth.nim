71e5fd2e64815396e9f0c12348f54eeb1b0c41f3
Brent Pedersen <bpederse@gmail.com>
2024-03-20 12:37:46 +0100

    bump version
    update binary to fix #224 by building with htslib v1.19.1

--- mosdepth.nim
+++ mosdepth.nim
@@ -782,7 +782,7 @@ when(isMainModule):
   when not defined(release) and not defined(lto):
     stderr.write_line "[mosdepth] WARNING: built in debug mode; will be slow"
 
-  let version = "mosdepth 0.3.6"
+  let version = "mosdepth 0.3.7"
   let env_fasta = getEnv("REF_PATH")
   var doc = format("""
   $version
--- mosdepth.nimble
+++ mosdepth.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.3.6"
+version       = "0.3.7"
 author        = "Brent Pedersen"
 description   = "fast depth"
 license       = "MIT"
